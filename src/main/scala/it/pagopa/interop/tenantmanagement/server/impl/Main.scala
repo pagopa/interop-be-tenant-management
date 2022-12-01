a6d40379b2d700a4d58e3060fef146d58db9d85b
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-12-01 16:21:22 +0100

    pin-2339: Disable projections

--- src/main/scala/it/pagopa/interop/tenantmanagement/server/impl/Main.scala
+++ src/main/scala/it/pagopa/interop/tenantmanagement/server/impl/Main.scala
@@ -45,7 +45,7 @@ object Main extends App with Dependencies {
 
       cluster.subscriptions ! Subscribe(listener, classOf[ClusterEvent.MemberEvent])
 
-      if (ApplicationConfiguration.projectionsEnabled) initProjections()
+//      if (ApplicationConfiguration.projectionsEnabled) initProjections()
 
       logger.info(renderBuildInfo(BuildInfo))
       logger.info(s"Started cluster at ${cluster.selfMember.address}")
